d7ca17e0c6b0a0d9d68b072de07475b4212baf8f
maulanairfanf <maulanairfanf@gmail.com>
2020-06-29 21:34:30 +0700

    article

---
+++ article.js
@@ -0,0 +1,46 @@
+// var request = new XMLHttpRequest();
+// request.onload = function() {
+//     var data = JSON.parse(this.responseText);
+//     console.log(data);
+// }
+// request.onerror = function() {
+//     console.log('Error :-S', error);
+// }
+// request.open('get', 'https://readerapi.codepolitan.com/articles', true);
+// request.send();
+
+// fetch('https://readerapi.codepolitan.com/articles')
+//     .then(function (response) {
+//         if (response.status !== 200) {
+//             console.log('Error : ' + response.status);
+//             return;
+//         }
+        
+//         response.json().then(function(data) {
+//             console.log(data);
+//         })
+//     }).catch(function(error) {
+//         console.log('Error : ' + error);
+//     });
+
+fetch('https://readerapi.codepolitan.com/articles')
+    .then(function (response) {
+        if (response.status !== 200) {
+            console.log('Error : ' + response.status);
+            // Method reject() akan membuat blok catch terpanggil
+            return Promise.reject(new Error(response.statusText));
+        } else {
+            // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+            return Promise.resolve(response);
+        }
+    }).then(function(response) {
+        // Mengembalikan sebuah Promise berupa objek/array JavaScript
+        // yang diubah dari teks JSON. 
+        return response.json();
+    }).then(function(data) {
+        // Objek/array JavaScript dari response.json() masuk lewat data.
+        console.log(data);
+    }).catch(function(error) {
+        // Parameter error berasal dari Promise.reject() 
+        console.log('Error : ' + error);
+    });
